eeb24db7adbe3f072e4040ecb2e115e8c140427e
Carina Carlos <121907876+cmlc717@users.noreply.github.com>
2023-06-23 14:42:19 -0400

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('postgres://localhost:5432/acme_schools_db', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/acme_schools_db', {
     logging: false
 });
 
